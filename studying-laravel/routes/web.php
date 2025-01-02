9b809c57496f5251b0642f7be6e8cece9b9d4cba
coeurl <coeurl@lavoscore.org>
2025-01-02 21:04:29 +0900

    routes/web.php

--- studying-laravel/routes/web.php
+++ studying-laravel/routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('global.index');
+})->name('/');
+
+Route::get('/mypage', function () {
+    return view('auth.mypage');
+})->name('mypage');
+
+Route::get('/contact', function () {
+    return view('global.contact');
+})->name('contact');
+
+Route::get('/law', function () {
+    return view('global.law');
+})->name('law');
+
+Route::get('/privary', function () {
+    return view('global.privacy');
+})->name('privacy');
+
+Route::get('/terms-of-use', function () {
+    return view('global.terms-of-use');
+})->name('terms-of-use');
