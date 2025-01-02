ef9c1b95b854a0876f2a9bf5afa76e8eb9ab5c72
coeurl <coeurl@lavoscore.org>
2025-01-02 20:22:10 +0900

    migration file; add_column_to_users_table

--- studying-laravel/database/migrations/2025_01_02_201901_add_column_to_users_table.php
+++ studying-laravel/database/migrations/2025_01_02_201901_add_column_to_users_table.php
@@ -12,7 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('name_kana')->after('name');
+            $table->date('birthday')->nullable()->after('name_kana');
+            $table->enum('gender', ['UNSET', 'MALE', 'FEMALE'])->default('UNSET')->after('birthday');
+            $table->string('address_zipcode', 16)->after('gender');
+            $table->string('address')->after('address_zipcode');
+            $table->string('phone_number', 32)->after('address');
+            $table->enum('role', ['USER', 'OWNER'])->default('USER')->after('phone_number');
+            $table->enum('status', ['REGISTERED', 'PROVISIONAL', 'BANNED', 'DELETED'])->default('PROVISIONAL')->after('role');
+            $table->softDeletes();
         });
     }
 
@@ -22,7 +30,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $columns = ['name_kana', 'birthday', 'gender', 'address_zipcode', 'address', 'phone_number', 'role', 'status'];
+            foreach ($columns as $column) {
+                $table->dropColumn($column);
+            }
+            $table->dropSoftDeletes();
         });
     }
 };
